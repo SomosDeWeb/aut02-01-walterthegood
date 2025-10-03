3181dd9c804bf1c1432f9620e5ed388eb6a8253e
walterthegood <waltersalmon200@gmail.com>
2025-10-03 16:59:09 +0100

    Solo el menu nada

--- AUT02_01/src/main/java/org/example/Main.java
+++ AUT02_01/src/main/java/org/example/Main.java
@@ -1,17 +1,49 @@
 package org.example;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Boolean salir = true;
+
+        do {
+
+            System.out.print(" === Gestor de Estudiantes === \n" +
+                    "1. Añadir estudiante \n"+
+                    "2. Listar estudiantes \n"+
+                    "3. Buscar por nombre \n"+
+                    "4. Calcular nota media general \n"+
+                    "5. Mostrar mejor estudiante \n"+
+                    "6. Salir \n"+
+                    "Seleccione opción: "
+            );
+
+            Integer opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 6:
+                    salir = false;
+                    break;
+
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+            }
+
+
+
+        } while (salir);
+
+        System.out.println("haz salido adios");
+
+        sc.close();
+
     }
 }
